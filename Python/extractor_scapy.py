bff6d4f57f1a1e0587b9a5ada787339ec5f246ec
Peter Ott <ott.peter.r@gmail.com>
2017-02-21 15:48:42 -0500

    Making a scapy demo

---
+++ Python/extractor_scapy.py
@@ -0,0 +1,22 @@
+import pcapy
+from scapy.all import *
+
+def getMACs(pathToPcap):
+    count = 0
+    packets = rdpcap(pathToPcap)
+
+    for packet in packets:
+        count = count + 1
+        print count
+        print packet.addr1
+        print
+
+
+
+
+def test():
+    getMACs("/Users/pott/Desktop/one.pcap")
+
+
+if __name__ == '__main__':
+    test()
