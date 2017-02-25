155d6135a15cb86ffe81e322f04a27367bcbf61c
Peter Ott <ott.peter.r@gmail.com>
2017-02-24 22:19:17 -0500

    Fixed up the reading Made load into reload() Added a few methods to get databases

--- Python/config/ConfigHelper.py
+++ Python/config/ConfigHelper.py
@@ -2,23 +2,55 @@ import ConfigParser
 import DefaultLoader
 import os
 
-defaultConfigPath = "trakr.cfg"
-
+defaultConfigPath = "./trakr.ini"
 config = ConfigParser.ConfigParser()
+loaded = False
+
+
+class ConfigHelper():
+    startUp()
+
+
 
 #loads up the config file, or a default if its missing
 def startUp():
-    #check if config is missing
+    if loaded:
+        return
+    reload()
+
+
+
+def reload():
+    # check if config is missing
     if not os.path.exists(defaultConfigPath):
-        #create it if needed
+        # create it if needed
         DefaultLoader.createDefaultConfig()
 
-    #now that we know it's there, load it up
+    # now that we know it's there, load it up
     __loadConfig(defaultConfigPath)
+
 
 #given the path to the config, load it
 def __loadConfig(path):
     global config
-    config.read(defaultConfigPath)
+    config.read(defaultConfigPath, 'r')
+    loaded = True
     return
 
+
+
+######################################
+########### access methods ###########
+######################################
+
+@staticmethod
+def getComprehensiveDatabasePath():
+    return config.get("db", "comprehensive_path")
+
+@staticmethod
+def getReducedDatabasePath():
+    return config.get("db", "reduced_path")
+
+@staticmethod
+def getGraphDatabasePath():
+    return config.get("db", "graph_path")
