c4db43010c3a17f8bb204f609a51e438ebdca13e
Peter Ott <ott.peter.r@gmail.com>
2017-02-25 02:39:00 -0500

    added some defaults for databases

--- Python/config/DefaultLoader.py
+++ Python/config/DefaultLoader.py
@@ -1,13 +1,36 @@
 #this loads up the default configurations upon first launch
 import ConfigParser
+import os
+
 
 defaultConfigPath = "./trakr.ini"
 
 def createDefaultConfig():
     file = open(defaultConfigPath, 'w')
     config = ConfigParser.ConfigParser()
+
+    #database section
+    config.add_section("db")
+    config.set("db", "rolling_path", (str(getFullTrakrPath()))+"/db/rolling.db")
+    config.set("db", "reduced_path", (str(getFullTrakrPath()))+"/db/reduced.db")
+    config.set("db", "graph_path", (str(getFullTrakrPath()))+"/db/graph.db")
+
+
+
+
+    #monitoring section
+    config.add_section("scanner")
+    config.set("scanner","observation_interval_seconds", 600)
+
+
+    print getFullTrakrPath()
+
     config.write(file)
 
+
+
+def getFullTrakrPath():
+    return os.getcwd()[:-7]
 
 
 
