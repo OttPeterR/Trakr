bf9cbd87a7c525171e8d0d3cda12ba05bb70bd00
Peter Ott <ott.peter.r@gmail.com>
2017-02-25 10:55:17 -0500

    Fixed self calling methods

--- Python/config/ConfigHelper.py
+++ Python/config/ConfigHelper.py
@@ -2,40 +2,49 @@ import ConfigParser
 import DefaultLoader
 import os
 
-defaultConfigPath = "./trakr.ini"
-config = ConfigParser.ConfigParser()
-loaded = False
-
-
 class ConfigHelper():
-    startUp()
+    global loaded
+    global configParser
+    global defaultConfigPath
+
+    loaded = False
+    configParser = ConfigParser.ConfigParser()
+    defaultConfigPath = "./trakr.ini"
+
+    #loads up the config file, or a default if its missing
+    def startUp(self):
+        global loaded
+        if loaded:
+            return
+        else:
+            self.reloadConfig()
 
 
 
-#loads up the config file, or a default if its missing
-def startUp():
-    if loaded:
+
+    def reloadConfig(self):
+        global configParser
+        global defaultConfigPath
+
+        # check if config is missing
+        if not os.path.exists(defaultConfigPath):
+            # create it if needed
+            DefaultLoader.createDefaultConfig()
+
+        # now that we know it's there, load it up
+        self.loadConfig()
+
+
+    #given the path to the config, load it
+    def loadConfig(self):
+        global configParser
+        global defaultConfigPath
+
+        configParser.read(defaultConfigPath)
+        loaded = True
         return
-    reload()
 
 
-
-def reload():
-    # check if config is missing
-    if not os.path.exists(defaultConfigPath):
-        # create it if needed
-        DefaultLoader.createDefaultConfig()
-
-    # now that we know it's there, load it up
-    __loadConfig(defaultConfigPath)
-
-
-#given the path to the config, load it
-def __loadConfig(path):
-    global config
-    config.read(defaultConfigPath, 'r')
-    loaded = True
-    return
 
 
 
@@ -43,14 +52,11 @@ def __loadConfig(path):
 ########### access methods ###########
 ######################################
 
-@staticmethod
-def getRollingDatabasePath():
-    return config.get("db", "rolling_path")
+    def getRollingDatabasePath(self):
+        return configParser.get("db", "rolling_path")
 
-@staticmethod
-def getReducedDatabasePath():
-    return config.get("db", "reduced_path")
+    def getReducedDatabasePath(self):
+        return configParser.get("db", "reduced_path")
 
-@staticmethod
-def getGraphDatabasePath():
-    return config.get("db", "graph_path")
+    def getGraphDatabasePath(self):
+        return configParser.get("db", "graph_path")
