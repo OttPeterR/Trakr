5aa06675366864c1c8766df9c6a5350fb8a9577f
Peter Ott <ott.peter.r@gmail.com>
2017-02-25 02:39:42 -0500

    Draft

---
+++ Python/Trakr.py
@@ -0,0 +1,14 @@
+from config import ConfigHelper
+
+
+if __name__ == '__main__':
+    #make sure the settings are all loaded up and good
+    ConfigHelper.startUp()
+
+    #ask user if they want to begin monitoring
+        #start it
+
+    #would you like to export information from the DB
+        #parameters
+            #specific MACs
+            #time frame
