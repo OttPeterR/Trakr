6c137a065302389d7a21300994b598a1c1e45e93
Peter Ott <ott.peter.r@gmail.com>
2017-03-01 00:17:17 -0500

    threads catch themself
    This makes sure the system won’t hang if a thread errors and never
    finished and thus never decrements the running thread count

--- Python/scanner/ScannerThread.py
+++ Python/scanner/ScannerThread.py
@@ -5,19 +5,22 @@ import ThreadKeeper
 
 def scan():
     ThreadKeeper.incrementThreadCount()
+    try:
+        print "New thread is scanning..."
+        captureDirectory = str(ConfigHelper.getTRAKrFullPath())
+        capturePath = captureDirectory+"/scanner/output/capture.pcap"
+        captureInterface = str(ConfigHelper.getWirelessCaptureInterface())
+        captureDuration = str(ConfigHelper.getCaptureDuration())
 
-    print "New thread is scanning..."
-    captureDirectory = str(ConfigHelper.getTRAKrFullPath())
-    captureInterface = str(ConfigHelper.getWirelessCaptureInterface())
-    captureDuration = str(ConfigHelper.getCaptureDuration())
-
-    # tshark-I -i CAPTURE_INTERFACE -a duration:CAPTURE_DURATION -w OUTPUT_FILE.pcap
-    call(["tshark",
-                     "-I",  #monitor mode
-                     "-i", captureInterface, # capture interface
-                     "-a", ("duration:"+captureDuration), # run time
-                     "-w", "capture.pcap"])
-
+        # tshark -I -i CAPTURE_INTERFACE -a duration:CAPTURE_DURATION -w OUTPUT_FILE.pcap
+        call(["tshark",
+                         "-I",  #monitor mode
+                         "-i", captureInterface, # capture interface
+                         "-a", ("duration:"+captureDuration), # run time
+                         "-w", capturePath])
+    except Exception, errmsg:
+        print "Scan thread failed while running:"
+        print errmsg
     ThreadKeeper.decrementThreadCount()
     return
 
