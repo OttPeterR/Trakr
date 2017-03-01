8cc852d48e0cc6a5e84dbdba1e69840a7d22d31d
Peter Ott <ott.peter.r@gmail.com>
2017-03-01 02:03:55 -0500

    Initial import

---
+++ Python/analysis/BehaviorReducerThread.py
@@ -0,0 +1,7 @@
+
+def analyze(filePath):
+    # parses through the rolling database and makes guesses as to when MACs enter and exit
+    # after computation, it gives its data to the BehaviorDatabase to insert it into the DB
+    return
+
+
