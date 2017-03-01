29acd87b2c3a7659c402132a2cf74721ba34f5dc
Peter Ott <ott.peter.r@gmail.com>
2017-03-01 00:38:40 -0500

    Some tweaks for the defaults

--- Python/config/DefaultLoader.py
+++ Python/config/DefaultLoader.py
@@ -29,12 +29,12 @@ def createDefaultConfig():
     # find a way to check the OS and then get the default interface
     # because this works for MacOS only
     config.set("scanner", "interface", "en0")
-    config.set("scanner","duration", 15)
+    config.set("scanner","duration", 60*10) #scan for 10 minutes
 
     #analysis section
     config.add_section("analysis")
-    config.set("analysis", "exit_seconds", 1800) # 30 minutes
-    config.set("analysis", "enter_period_seconds", 180)
+    config.set("analysis", "exit_seconds", 60*30) # 30 minutes
+    config.set("analysis", "enter_period_seconds", 60*3) # 3 minutes
 
     # enter_period_second will be divided into enter_subperiod groups
     # and there must be at least one wifi observation in each of those
