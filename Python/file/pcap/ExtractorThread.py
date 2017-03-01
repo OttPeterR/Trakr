1cf1930bc177586335d4ba21bdef04cafe9a2b85
Peter Ott <ott.peter.r@gmail.com>
2017-03-01 13:16:47 -0500

    Do a little bit of analysis

--- Python/file/pcap/ExtractorThread.py
+++ Python/file/pcap/ExtractorThread.py
@@ -16,7 +16,27 @@ def extract(filePath):
     for packet in packets:
         observations += [Observation.makeObservation(packet)]
 
-    print len(observations)
+
+    dict = getUniqueMACs(observations)
+
+    print str(len(packets))+" - analysis done"
 
     ThreadKeeper.decrementThreadCount()
     return
+
+
+
+
+def getUniqueMACs(observations):
+    ind = 0
+    dict = {}
+    address = ""
+
+    for o in observations:
+        # is this a new mac address?
+        if o.mac not in dict:
+            dict[address] = ind
+            ind = ind + 1
+
+    print "unique: " + str(len(dict))
+    return dict
