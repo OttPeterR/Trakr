54744fcb1ed59d0a79b23b729b9ed5b37d915c93
Peter Ott <ott.peter.r@gmail.com>
2017-03-01 02:45:51 -0500

    breaking functions down into helpers

--- Python/TRAKr.py
+++ Python/TRAKr.py
@@ -28,16 +28,19 @@ def TRAKr(app):
 
     # handling run parameters
 
-    if TRAKr.params.scan:
-        # root check
-        if os.geteuid() == 0:
-            # we're good to go, let's scan
+    if TRAKr.params.run:
+        run()
+    else:
+        if TRAKr.params.scan:
             needToWait = True
-            Scanner.beginScan()
-        else:
-            # too bad
-            print "Please run as root to capture packets. Exiting..."
-            os._exit(0)
+            scan()
+
+        #if TRAKr.params.analyze:
+        #    analyze()
+
+
+
+    #done with all the parameters, now we just finish up
 
     if needToWait:
         # giving the params a chance to start their threads
@@ -47,6 +50,32 @@ def TRAKr(app):
     print("[TRAKr] - Shutting Down")
 
 
+
+# Helper functions to do the things
+
+def scan():
+    # root check
+    if os.geteuid() == 0:
+        # we're good to go, let's scan, this will loop infinitely
+        Scanner.beginScan()
+    else:
+        # sucks. no scanning today
+        print "Please run as root to capture packets. Exiting..."
+        os._exit(0)
+    return
+
+
+def analyze():
+    # call analysis
+
+    return
+
+
+def run():
+    scan()
+    analyze()
+    return
+
 # for operational things, use a parameter that gets set
 TRAKr.add_param("-r", "--run", help="this starts capture and analysis processing all-in-one. Needs root permissions",
                 action='store_true')
@@ -54,6 +83,12 @@ TRAKr.add_param("-s", "--scan", help="begin scanning and saving to the database"
 TRAKr.add_param("-reset", "--reset", help="resets the config file to defaul", action='store_true')
 TRAKr.add_param("-d", "--export_database", help="this exports the graph.db and reduced.db into the /export dir",
                 action='store_true', default=False)
+
+#TODO params to add:
+#   -i import pcap from file or directory for analysis, remember to ask for lat/long
+#   -a run analysis
+
+
 
 if __name__ == '__main__':
     TRAKr.run()
