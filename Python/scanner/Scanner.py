ee749fc17863f5be124165295b060b23a1eb8641
Peter Ott <ott.peter.r@gmail.com>
2017-03-03 23:23:59 -0500

    Will not time out while waiting for user input
    Probably not the best solution, but it’s a quick fix

--- Python/scanner/Scanner.py
+++ Python/scanner/Scanner.py
@@ -1,15 +1,14 @@
 import thread
 from scanner import ScannerThread
+import ThreadKeeper
 
 def start(alsoAnalyze):
 
+    ThreadKeeper.incrementThreadCount()
     lat = raw_input("Please input Latitude:")
-    #lat = 0
-
     long = raw_input("Please input Longitude:")
-    #long = 0
     ScannerThread.scanLoop(alsoAnalyze, lat, long)
-
+    ThreadKeeper.decrementThreadCount()
 
 def beginScan(alsoAnalyze):
     try:
