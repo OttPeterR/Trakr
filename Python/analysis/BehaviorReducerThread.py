2410b8618f42e7657ba17ff4f3e6e3a948797e72
Peter Ott <ott.peter.r@gmail.com>
2017-03-20 18:04:56 -0400

    Putting analysis into its own method

--- Python/analysis/BehaviorReducerThread.py
+++ Python/analysis/BehaviorReducerThread.py
@@ -4,6 +4,8 @@ from config import ConfigHelper
 
 
 def analyze():
+    global action_exit, action_notice
+
     # parses through the rolling database and makes guesses as to when MACs enter and exit
     # after computation, it gives its data to the BehaviorDatabase to insert it into the DB
 
@@ -15,64 +17,67 @@ def analyze():
     # loop through that array ^ and add labels to the items of notice, enter, present, exit
     # reduce the array by taking out all "present" items that are between present items
 
-    behaviorDB = BehaviorDatabaseHelper.connect()
-    rollingDB = RollingDatabaseHelper.connect()
+    behaviorDBConnection = BehaviorDatabaseHelper.connect()
+    rollingDBConnection = RollingDatabaseHelper.connect()
 
-    uniques = BehaviorDatabaseHelper.getUniques(behaviorDB)
+    uniques = BehaviorDatabaseHelper.getUniques(behaviorDBConnection)
 
     action_notice = BehaviorDatabaseHelper.type_notice
     action_exit = BehaviorDatabaseHelper.type_exit
 
-    for u in uniques:
-        observations = RollingDatabaseHelper.getObservationsOfAddress(rollingDB, u)
-        observation_actions = []  # this will hold the entry/exit actions
+    for address in uniques:
+        observationsOfAddress = RollingDatabaseHelper.getObservationsOfAddress(rollingDBConnection, address)
 
-        if len(observations) > 1:
+        reduceBehaviorsForAddress(address, observationsOfAddress, behaviorDBConnection, rollingDBConnection)
 
-            previous_state = __loadState(behaviorDB, u)
-
-            for o in range(len(observations) - 1):
-                dist = observations[o + 1].time - observations[o].time
-
-                # find what the state is
-
-                if dist > 30:  # ConfigHelper.getExitTime():
-                    current_state = action_notice
-                else:
-                    current_state = action_exit
-
-                # check if a new state change needs to be added to the list
-                if current_state == action_notice and previous_state == action_exit:
-                    observation_actions += [(action_notice, observations[o + 1].time)]
-                elif current_state == action_exit and previous_state == action_notice:
-                    observation_actions += [(action_exit, observations[o].time)]
-                    observation_actions += [(action_notice, observations[0+1].time)]
-                    o = o+1
-
-                # make the current state usable for next iteration
-                previous_state = current_state
-
-
-
-            if len(observation_actions) > 1:
-                print u
-                print observation_actions
-
-        elif len(observations) == 1:
-            # there is only one observation, just handle it as a notice
-            observation_actions += [(action_notice, observations[0].time)]
-        else:
-            # no observations: nothing to do, move along to the next iteration
-            continue
-
-        if len(observation_actions) != 0:
-            for oa in observation_actions:
-                BehaviorDatabaseHelper.addBehavior(behaviorDB, u, oa[0], oa[1], 0, 0)
-
-    behaviorDB.commit()
-    behaviorDB.close()
-    rollingDB.close()
+    # commit changes to behavior db and close out connections
+    behaviorDBConnection.commit()
+    behaviorDBConnection.close()
+    rollingDBConnection.close()
     return
+
+
+def reduceBehaviorsForAddress(address, observations, behaviorDB, rollingDB):
+    global action_exit, action_notice
+
+    observation_actions = []  # this will hold the entry/exit actions
+
+    if len(observations) > 1:
+
+        previous_state = __loadState(behaviorDB, address)
+
+        for o in range(len(observations) - 1):
+            dist = observations[o + 1].time - observations[o].time
+
+            # find what the state is
+
+            if dist > 30:  # ConfigHelper.getExitTime():
+                current_state = action_notice
+            else:
+                current_state = action_exit
+
+            # check if a new state change needs to be added to the list
+            if current_state == action_notice and previous_state == action_exit:
+                observation_actions += [(action_notice, observations[o + 1].time)]
+            elif current_state == action_exit and previous_state == action_notice:
+                observation_actions += [(action_exit, observations[o].time)]
+                observation_actions += [(action_notice, observations[0 + 1].time)]
+                o = o + 1
+
+            # make the current state usable for next iteration
+            previous_state = current_state
+
+    elif len(observations) == 1:
+        # there is only one observation, just handle it as a notice
+        observation_actions += [(action_notice, observations[0].time)]
+    else:
+        # no observations --> nothing to do, move along
+        return
+
+    # putting the observations into the behavior database
+    for actions in observation_actions:
+        # actions is a tuple
+        BehaviorDatabaseHelper.addBehavior(behaviorDB, address, actions[0], actions[1], 0, 0)
 
 
 def __loadState(connection, addr):
