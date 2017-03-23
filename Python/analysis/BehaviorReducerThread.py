8255b726abf2510b36d55245b2f6029e014afb6c
Peter Ott <ott.peter.r@gmail.com>
2017-03-22 20:06:37 -0400

    Started on making time observations
    Warning: broken - configparser is being weird

--- Python/analysis/BehaviorReducerThread.py
+++ Python/analysis/BehaviorReducerThread.py
@@ -5,25 +5,15 @@ from config import ConfigHelper
 
 def analyze():
     global action_exit, action_notice
+    action_notice = BehaviorDatabaseHelper.type_notice
+    action_exit = BehaviorDatabaseHelper.type_exit
 
-    # parses through the rolling database and makes guesses as to when MACs enter and exit
-    # after computation, it gives its data to the BehaviorDatabase to insert it into the DB
-
-    # naive solution
-
-    # m loop unique MACs
-    # boolean flags as to the status of m, notice -> enter -> present -> exit -> repeat
-    # get all from db where MAC = m and put it in an array
-    # loop through that array ^ and add labels to the items of notice, enter, present, exit
-    # reduce the array by taking out all "present" items that are between present items
 
     behaviorDBConnection = BehaviorDatabaseHelper.connect()
     rollingDBConnection = RollingDatabaseHelper.connect()
 
     uniques = BehaviorDatabaseHelper.getUniques(behaviorDBConnection)
-
-    action_notice = BehaviorDatabaseHelper.type_notice
-    action_exit = BehaviorDatabaseHelper.type_exit
+    timeSlots = makeTimeSlots()
 
     for address in uniques:
         observationsOfAddress = RollingDatabaseHelper.getObservationsOfAddress(rollingDBConnection, address)
@@ -37,10 +27,13 @@ def analyze():
         # getting all actions for this address (there could be other ones in
         # the DB that were not in this capture)
         actions = BehaviorDatabaseHelper.getAllActionsForAddress(behaviorDBConnection, address)
-        for action in actions:
-            continue
 
-    # commit changes to behavior db and close out connections
+        if len(actions) > 1:
+            for action in actions:
+                # each action is a tuple of (TYPE, TIME)
+                continue
+
+    # commit changes to behavior db and close out both connections
     behaviorDBConnection.commit()
     behaviorDBConnection.close()
     rollingDBConnection.close()
@@ -77,9 +70,6 @@ def makeEntriesAndExitsForAddress(address, observations, behaviorDB, rollingDB):
             # make the current state usable for next iteration
             previous_state = current_state
 
-        print address
-        print observation_actions
-
     elif len(observations) == 1:
         # there is only one observation, just handle it as a notice
         observation_actions += [(action_notice, observations[0].time)]
@@ -87,12 +77,19 @@ def makeEntriesAndExitsForAddress(address, observations, behaviorDB, rollingDB):
         # no observations --> nothing to do, move along
         return
 
+    print address
+    print observation_actions
+
     # putting the observations into the behavior database
     for actions in observation_actions:
         # actions is a tuple
         BehaviorDatabaseHelper.addBehavior(behaviorDB, address, actions[0], actions[1], 0, 0)
 
-
+def makeTimeSlots():
+    # returns an array of the size of time slots that should be created
+    slotsPerHour = ConfigHelper.getHourSegments()
+    backTrackHours = ConfigHelper.getNumBackTrackHours()
+    return [0]*slotsPerHour*backTrackHours
 
 def __loadState(connection, addr):
     return BehaviorDatabaseHelper.getMostRecentStatus(connection, addr)
