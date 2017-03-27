0af9b7cb4eac7581829dae067aea832af1a75e23
Peter Ott <ott.peter.r@gmail.com>
2017-03-27 18:42:54 -0400

    Just some ideas

--- Python/file/export/CSVWriter.py
+++ Python/file/export/CSVWriter.py
@@ -1,5 +1,11 @@
 import csv
 from database.relational import BehaviorDatabaseHelper
 
+
 def export(file_path):
+    # export a csv file that will follow this structure:
+    # time stamp, num users within from now to next time stamp, num new users seen, avg. prev. visits for not new users
+    # time stamp will increment starting at oldest and going forward
+
+    # most of this data can be taken directly from the database
     pass
