89308a40ebbe951f1545a515b037f9f3fc0cca01
Peter Ott <ott.peter.r@gmail.com>
2017-04-20 01:01:30 -0400

    Extract from directory

--- Python/file/pcap/Extractor.py
+++ Python/file/pcap/Extractor.py
@@ -1,14 +1,26 @@
 import thread
 
 import ExtractorThread
+import os
 
 
 def start(filePath, lat, long, allowDeletion):
     ExtractorThread.extract(filePath, lat, long, allowDeletion)
 
-def ExtractFromFile(filePath, lat, long, allowDeletion=True):
+def extractFromFile(filePath, lat, long, allowDeletion=True):
     try:
         thread.start_new_thread(start, (filePath, lat, long, allowDeletion))
+    except Exception, errmsg:
+        print "Extraction thread failed to start:"
+        print errmsg
+
+
+
+def extractFromDir(path, lat, long, allowDeletion=True):
+    try:
+        for f in os.listdir(path):
+            if f.endswith(".pcap"):
+                start(path+f, lat, long, allowDeletion)
     except Exception, errmsg:
         print "Extraction thread failed to start:"
         print errmsg
