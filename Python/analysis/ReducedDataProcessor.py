a7b0a46a9fecfd89c651b04d269deae77071184b
Peter Ott <ott.peter.r@gmail.com>
2017-04-20 16:39:44 -0400

    Initial import

---
+++ Python/analysis/ReducedDataProcessor.py
@@ -0,0 +1,15 @@
+import thread
+from management import ThreadKeeper
+import ReducedDataProcessorThread
+
+def __export():
+    ThreadKeeper.incrementThreadCount()
+    ReducedDataProcessorThread.analyze()
+    ThreadKeeper.decrementThreadCount()
+
+def beginExport():
+    try:
+        thread.start_new_thread(__export(), ())
+    except Exception, errmsg:
+        print "Export failed to start:"
+        print errmsg
