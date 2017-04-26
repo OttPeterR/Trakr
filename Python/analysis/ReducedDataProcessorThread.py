376b6ee9ba69368fed363bccce34fa0f1f47331f
Peter Ott <ott.peter.r@gmail.com>
2017-04-26 12:58:08 -0400

    begun work on filtering out randomized MACs

--- Python/analysis/ReducedDataProcessorThread.py
+++ Python/analysis/ReducedDataProcessorThread.py
@@ -1,4 +1,5 @@
 from database.relational import BehaviorDatabaseHelper
+from database.relational.BehaviorDatabaseHelper import SimpleAction
 from config import ConfigHelper
 
 
@@ -18,14 +19,10 @@ def makeExportData():
     current_entries = 0
     current_exits = 0
     current_population = 0
-    current_addresses = []  # make me a dictionary of (address, time)
-    # each time will be an entry time and when called it will be called
-    # with the action that is the corresponding exit
-    # from there a timespan can be calculated and decided if it should be kept
-    # or not, use config: entry_time
 
+    filtered_actions = all_actions#__filterActions(all_actions)
 
-    for action in all_actions:
+    for action in filtered_actions:
         # this is guaranteed to hit on the first packet
         # check if a new bracket needs to be created
         if action.time > time_end:
@@ -48,10 +45,50 @@ def makeExportData():
         # entry
         if action.action == 1:
             current_entries += 1
-           # current_addresses += [(action.address, action.time)]
+
         # exit
         else:
             current_exits += 1
-#            current_addresses.remove(action.address)
-
     return csv_data[1:]
+
+
+def __filterActions(all_actions):
+    filtered_actions = []
+    entries = []
+    for a in all_actions:
+        # if its an entry, just log it in the list
+        if a.action == 1:
+            entries += [EntryAction(a.time, a.address)]
+        # if it's an exit, we need to see if we're cutting it out
+        else:
+            prev = __findEntryTime(a.address, entries)
+            timespan = a.time - prev
+            # should we keep it?
+            if timespan > ConfigHelper.getEntryTime():
+                filtered_actions += [SimpleAction(1, a.time, a.address)]
+                filtered_actions += [SimpleAction(0, prev, a.address)]
+
+    # now everything in filtered_actions are likely to be non-randomized MAC address devices
+    # need to sort by time
+    filtered_actions.sort()
+
+    return filtered_actions
+
+
+def __findEntryTime(address, entries):
+    for e in entries:
+        if e.address == address:
+            return e.time
+    return None
+
+
+class EntryAction:
+    time = 0
+    address = 0
+
+    def __init__(self, time, address):
+        self.time = time
+        self.address = address
+
+    def __eq__(self, other):
+        return self.address == other.address
